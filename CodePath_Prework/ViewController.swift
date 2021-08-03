46ec7b6686bde296bf378ce580cc4b3be7d7660e
Ilia Zlatkin <ilya.zlatkin@gmail.com>
2021-08-02 23:13:45 -0400

    init commit

--- CodePath_Prework/ViewController.swift
+++ CodePath_Prework/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateTip(_ sender: Any) {
+        //get bill amount from text filed
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        // get total tip by mult tip * tipPercentage
+        let tipPercantages = [0.15, 0.18, 0.2]
+        let tip = bill * tipPercantages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        // update tip amount label
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        // update total amount
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
 }
 
